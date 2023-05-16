289d42ee41c113e8984441f0d6121d69721e854c
PedroBernardes01 <133461209+PedroHenriqueSBer@users.noreply.github.com>
2023-05-16 20:28:37 -0300

    .

--- Atex/Class1.cs
+++ Atex/Class1.cs
@@ -8,51 +8,48 @@ using MySql.Data.MySqlClient;
 
 namespace Atex
 {
-    namespace Mysql_Object
+    public class WorkBench
     {
-        public class WorkBench
+        private MySqlConnection Database;
+        public WorkBench(string host, string username, string database, string password) =>
+            Database = new MySqlConnection($"server={host};User Id={username};database={database}; password={password}");
+        public bool Quarry(string command)
         {
-            private MySqlConnection Database;
-            public WorkBench(string host, string username, string database, string password) =>
-                Database = new MySqlConnection($"server={host};User Id={username};database={database}; password={password}");
-            public bool Quarry(string command)
+            try
             {
-                try
-                {
-                    Database.Open();
-                    MySqlCommand msqlcmd = new MySqlCommand(command, Database);
-                    msqlcmd.ExecuteNonQuery();
-                    Database.Close();
-                    return true;
-                }
-                catch (MySql.Data.MySqlClient.MySqlException)
-                {
-                    return false;
-                }
+                Database.Open();
+                MySqlCommand msqlcmd = new MySqlCommand(command, Database);
+                msqlcmd.ExecuteNonQuery();
+                Database.Close();
+                return true;
             }
-            public List<string[]> SELECT(string command, int QntTableElem)
+            catch (MySql.Data.MySqlClient.MySqlException)
             {
-                try
+                return false;
+            }
+        }
+        public List<string[]> SELECT(string command, int QntTableElem)
+        {
+            try
+            {
+                Database.Open();
+                MySqlCommand msqlcmd = new MySqlCommand(command, Database);
+                MySqlDataReader mdr = msqlcmd.ExecuteReader();
+                string[] aux;
+                List<string[]> ListAux = new List<string[]>();
+                while (mdr.Read())
                 {
-                    Database.Open();
-                    MySqlCommand msqlcmd = new MySqlCommand(command, Database);
-                    MySqlDataReader mdr = msqlcmd.ExecuteReader();
-                    string[] aux;
-                    List<string[]> ListAux = new List<string[]>();
-                    while (mdr.Read())
-                    {
-                        aux = new string[QntTableElem];
-                        for (int i = 0; i < aux.Length; i++)
-                            aux[i] = mdr.GetString(i);
-                        ListAux.Add(aux);
-                    }
-                    Database.Close();
-                    return ListAux;
+                    aux = new string[QntTableElem];
+                    for (int i = 0; i < aux.Length; i++)
+                        aux[i] = mdr.GetString(i);
+                    ListAux.Add(aux);
                 }
-                catch (MySql.Data.MySqlClient.MySqlException)
-                {
-                    return new List<string[]>();
-                }
+                Database.Close();
+                return ListAux;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException)
+            {
+                return new List<string[]>();
             }
         }
     }
